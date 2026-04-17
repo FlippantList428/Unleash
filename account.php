da30642d19271ffdc6fa90ecd6974b5086277c14
FlippantList428 <kacperek18210@gmail.com>
2026-04-17 22:47:56 +0200

    Update index.html and styles.css for improved layout and navigation; add account.php for user account management

---
+++ account.php
@@ -0,0 +1,22 @@
+<?php 
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="icon" type="image/png" href="favicon.png">
+    <title>Unleash</title>
+</head>
+<body>
+    <nav>
+        <a class="odnosnik" href="index.html">🏠 Strona główna</a>
+        <a class="odnosnik" href="index.html/#news">🗞️ Nowości</a>
+        <a class="odnosnik" href="#">💬 Społeczność</a>
+        <a class="odnosnik" href="#">❤️‍🔥 Popularne</a>
+        <a class="odnosnik" href="account.php">👨‍🦱 Konto</a>
+    </nav>
+</body>
+</html>
