8e0599d7a2a7cc82d5afce2c822cb70b47588752
FlippantList428 <kacperek18210@gmail.com>
2026-04-17 22:03:29 +0200

    Add initial database creation script with user table definition

---
+++ create_db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS Unleash;
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_polish_ci
+USE Unleash;
+
+CREATE TABLE IF NOT EXISTS uzytkownicy (
+    id INT primary key auto_increment,
+    login varchar(50) NOT null UNIQUE,
+    haslo varchar(255) NOT null,
+    email varchar(100) NOT null UNIQUE,
+    telefon varchar(15) UNIQUE,
+    rejestracja DATETIME DEFAULT CURRENT_TIMESTAMP
+)
