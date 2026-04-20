0fc15fa0107ea4ec6ca5cf899728286c1c8867a4
Kacper Wróżek <kacperek18210@gmail.com>
2026-04-20 19:38:30 +0200

    Refactor login page structure and enhance styling for improved user experience

--- login.php
+++ login.php
@@ -26,13 +26,15 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany'] == true) {
         <a class="odnosnik" href="#trendy">❤️‍🔥 Popularne</a>
         <a class="odnosnik" href="account.php">👨‍🦱 Konto</a>
     </nav>
-    <h2>Zaloguj się!</h2>
-    <form action="login_process.php" method="POST" style="margin-top: 20px;" class="logowanie">
-        <input type="text" name="login" placeholder="Login" required><br><br>
-        <input type="password" name="password" placeholder="Hasło" required><br><br>
-        <button type="submit">Zaloguj się</button>
-    </form>
-    <p>Nie masz jeszcze konta? <a href="register.php">Zarejestruj się tutaj</a></p>
+    <section class="login">
+        <h2>Zaloguj się!</h2>
+        <form action="login_process.php" method="POST" style="margin-top: 20px;" class="logowanie">
+            <input type="text" name="login" placeholder="Login" required><br><br>
+            <input type="password" name="password" placeholder="Hasło" required><br><br>
+            <button type="submit">Zaloguj się</button>
+        </form>
+        <p>Nie masz jeszcze konta? <a href="register.php">Zarejestruj się tutaj</a></p>
+    </section>
     <script src="scripts/script.js"></script>
 </body>
 </html>
